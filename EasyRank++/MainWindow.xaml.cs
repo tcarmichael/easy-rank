0dbbf2ca296da1479b7d1943906796ae4e20dd2d
Tandy Carmichael <tcarmichael@frontier.com>
2019-04-13 19:57:52 -0500

    Add project files.

---
+++ EasyRank++/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Timers;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace EasyRank__
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private MessageSender keySender;
+
+        private Timer timer;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // 20 key presses per second
+            double keyDelay_s = 1000.0 / 20.0;
+
+            timer = new Timer(keyDelay_s);
+            timer.Elapsed += RepeatKey;
+            timer.AutoReset = true;
+        }
+
+        private void StartButton_Checked(object sender, RoutedEventArgs e)
+        {
+            // Retrieve the StarCraft II processes.
+            var processes = Process.GetProcessesByName("SC2_x64");
+
+            var hWnd = processes.First().MainWindowHandle;
+
+            // Make sure the user entered a key to press.
+            if (selectedKey.Text.Length != 1)
+                return;
+
+            char keyCode = selectedKey.Text.Single();
+
+            keySender = new MessageSender(hWnd, keyCode);
+
+            keySender.SendDown(false);
+
+            timer.Start();
+        }
+
+        private void StartButton_Unchecked(object sender, RoutedEventArgs e)
+        {
+            timer.Stop();
+            keySender.SendUp();
+        }
+
+        private void RepeatKey(object sender, ElapsedEventArgs e)
+        {
+            keySender.SendDown(true);
+        }
+    }
+}
---
+++ EasyRank++/MessageSender.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace EasyRank__
+{
+    public class MessageSender
+    {
+        [DllImport("user32.dll")]
+        public static extern IntPtr PostMessage(IntPtr hWnd, uint Msg, UIntPtr wParam, uint lParam);
+
+        const uint WM_KEYDOWN = 0x100;
+        const uint WM_KEYUP = 0x0101;
+
+        private static readonly Dictionary<char, byte> SCAN_CODE_LOOKUP = new Dictionary<char, byte>
+        {
+            { 'Q', 0x10 },
+            { 'W', 0x11 },
+            { 'E', 0x12 },
+            { 'R', 0x13 },
+            { 'T', 0x14 },
+            { 'Y', 0x15 },
+            { 'U', 0x16 },
+            { 'I', 0x17 },
+            { 'O', 0x18 },
+            { 'P', 0x19 },
+            { 'A', 0x1E },
+            { 'S', 0x1F },
+            { 'D', 0x20 },
+            { 'F', 0x21 },
+            { 'G', 0x22 },
+            { 'H', 0x23 },
+            { 'J', 0x24 },
+            { 'K', 0x25 },
+            { 'L', 0x26 },
+            { 'Z', 0x2C },
+            { 'X', 0x2D },
+            { 'C', 0x2E },
+            { 'V', 0x2F },
+            { 'B', 0x30 },
+            { 'N', 0x31 },
+            { 'M', 0x32 },
+        };
+
+        private readonly IntPtr hWnd;
+        private readonly char keyCode;
+
+        private readonly uint lParamDown;
+        private readonly uint lParamDownRepeat;
+        private readonly uint lParamUp;
+
+        public MessageSender(IntPtr hWnd, char keyCode)
+        {
+            this.hWnd = hWnd;
+            this.keyCode = char.ToUpper(keyCode);
+
+            var scanCode = SCAN_CODE_LOOKUP[keyCode];
+
+            // Construct the lParam values.
+            // See: https://docs.microsoft.com/en-us/windows/desktop/inputdev/wm-keydown.
+
+            // The first KEYDOWN message only sends the scan code.
+            lParamDown = (uint)(scanCode << 16);
+
+            // Repeated KEYDOWN messages set the repeat count and previous key state to 1.
+            lParamDownRepeat = lParamDown | (1u) | (1u << 30);
+
+            // KEYUP messages set the transition state to 1.
+            lParamUp = lParamDownRepeat | (1u << 31);
+        }
+
+        public void SendDown(bool isRepeat)
+        {
+            var lParam = (isRepeat) ? lParamDownRepeat : lParamDown;
+            PostMessage(hWnd, WM_KEYDOWN, (UIntPtr)keyCode, lParam);
+        }
+
+        public void SendUp()
+        {
+            PostMessage(hWnd, WM_KEYDOWN, (UIntPtr)keyCode, lParamUp);
+        }
+    }
+}
